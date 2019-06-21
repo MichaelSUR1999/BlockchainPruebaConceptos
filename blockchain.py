0d501e1ffbbe6d20b809c59f6aff5777ffce2c70
Michael <michaelurrego@hotmail.com>
2019-06-21 11:43:44 -0500

    Agregar funciones
    Registro de nodos, validación de cadenas, resolver conflictos

--- blockchain.py
+++ blockchain.py
@@ -7,10 +7,86 @@ from uuid import uuid4
 import requests
 from flask import Flask , jsonify , request
 
-class blockchain:
+class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+    def register_node ( self , address ) :
+        """
+        Añadir un nuevo nodo a la lista de nodos
+        :param address: Dirección del nodo.
+        """
+        parsed_url = urlparse ( address )
+        if parsed_url.netloc :
+            self.nodes.add ( parsed_url.netloc )
+        elif parsed_url.path :
+            # Acepta una URL sin esquema.
+            self.nodes.add ( parsed_url.path )
+        else :
+            raise ValueError ( 'Invalid URL' )
+
+    def valid_chain ( self , chain ) :
+        """
+        Determinar si una cadena de bloqueo determinada es válida
+        :param chain: Una cadena de bloqueo
+        :return: Verdadero si es válido, falso si no lo es
+        """
+
+        last_block = chain [ 0 ]
+        current_index = 1
+
+        while current_index < len ( chain ) :
+            block = chain [ current_index ]
+            print ( f'{last_block}' )
+            print ( f'{block}' )
+            print ( "\n-----------\n" )
+            # Comprobar que el hash del bloque es correcto
+            last_block_hash = self.hash ( last_block )
+            if block [ 'previous_hash' ] != last_block_hash :
+                return False
+
+            # Verifique que la Prueba de Trabajo sea correcta
+            if not self.valid_proof ( last_block [ 'proof' ] , block [ 'proof' ] , last_block_hash ) :
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts ( self ) :
+        """
+        Este es nuestro algoritmo de consenso, resuelve conflictos.
+        reemplazando nuestra cadena por la más larga de la red.
+        :return: True si nuestra cadena fue reemplazada, Falss si no
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # Sólo buscamos cadenas más largas que las nuestras.
+        max_length = len ( self.chain )
+
+        # Agarrar y verificar las cadenas de todos los nodos de nuestra red
+        for node in neighbours :
+            response = requests.get ( f'http://{node}/chain' )
+
+            if response.status_code == 200 :
+                length = response.json ( ) [ 'length' ]
+                chain = response.json ( ) [ 'chain' ]
+
+                # Compruebe si la longitud es mayor y si la cadena es válida
+                if length > max_length and self.valid_chain ( chain ) :
+                    max_length = length
+                    new_chain = chain
+
+        # Reemplazar nuestra cadena si descubrimos una cadena nueva y válida más larga que la nuestra
+        if new_chain :
+            self.chain = new_chain
+            return True
+
+        return False
 
     def new_block ( self , proof , previous_hash ) :
         """
@@ -160,6 +236,17 @@ def full_chain () :
         'length' : len ( blockchain.chain ) ,
     }
     return jsonify ( response ) , 200
+
+if __name__ == '__main__' :
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser ( )
+    parser.add_argument ( '-p' , '--port' , default = 5000 , type = int , help = 'port to listen on' )
+    args = parser.parse_args ( )
+    port = args.port
+
+    #app.debug = True
+    app.run ( host = 'localhost' , port = port )
 
 
 
